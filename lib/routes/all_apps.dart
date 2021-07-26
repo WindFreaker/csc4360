8901169372cebae8a8bffcbe2d07b642ebb75438
WindFreaker <windfreaker@gmail.com>
2021-07-26 01:03:51 -0400

    Created new routes
    Most empty, but favorite apps has some content. Just visual testing for now.

---
+++ lib/routes/all_apps.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AllApps extends StatefulWidget {
+  const AllApps({Key? key}) : super(key: key);
+
+  @override
+  _AllAppsState createState() => _AllAppsState();
+}
+
+class _AllAppsState extends State<AllApps> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
---
+++ lib/routes/detailed_status.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DetailedStatus extends StatefulWidget {
+  const DetailedStatus({Key? key}) : super(key: key);
+
+  @override
+  _DetailedStatusState createState() => _DetailedStatusState();
+}
+
+class _DetailedStatusState extends State<DetailedStatus> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
